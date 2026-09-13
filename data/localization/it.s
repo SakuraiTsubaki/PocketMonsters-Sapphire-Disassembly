86638c19225eeefb252ae7ccb73d3b70eaaeec5c
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 21:15:59 +0900

    Add Italian extended localization metadata source

---
+++ data/localization/it.s
@@ -0,0 +1,6 @@
+.syntax unified
+.section .rodata
+.include "data/localization_metadata.inc"
+
+/* AXPI Italian: byte-identical metadata block in Rev 0 and Rev 1. */
+emit_localization_metadata 4, 0x081E9F80, 0x081EB420, 0x081EC1E0, 0x081ECFA0, 0x083BC618, 0x083BCCF8, 0x083BCEB0, 0x081F8D98, 0x081F9F4C, 0x083EC3B8
