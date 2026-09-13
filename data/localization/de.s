fab5862c365bc297b1f07242419232a3fd1a8122
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 21:15:42 +0900

    Add German extended localization metadata source

---
+++ data/localization/de.s
@@ -0,0 +1,6 @@
+.syntax unified
+.section .rodata
+.include "data/localization_metadata.inc"
+
+/* AXPD German: observed in Rev 1 reference ROM. */
+emit_localization_metadata 5, 0x081F5264, 0x081F6704, 0x081F74C4, 0x081F8284, 0x083C7B9C, 0x083C827C, 0x083C8434, 0x0820407C, 0x08205230, 0x083F7B5C
